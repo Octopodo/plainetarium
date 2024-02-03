e5e72c532fa937ea60ba500b5381191eb4a4dfa9
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-02-03 14:31:09 +0100

    Implemented rudimentary copyToClipboard

---
+++ src/composables/useCopyToClipboard/useCopyToClipboard.ts
@@ -0,0 +1,27 @@
+import { ref } from 'vue'
+
+import { type Control } from '@/components/playgrounds/BasePlayground/types'
+
+export function useCopyToClipboard() {
+  const outputString = ref('')
+
+  const generateOutputString = (elementName: string, controls: Control[]) => {
+    let result = '<' + elementName + '\n'
+    for (const control of controls) {
+      result += ':' + control.modelName + '="' + control.model.value + '"\n'
+    }
+    result += '/>\n'
+    outputString.value = result
+  }
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(outputString.value)
+      console.log('Copied to clipboard')
+    } catch (err) {
+      console.error('Failed to copy text: ', err)
+    }
+  }
+
+  return { outputString, generateOutputString, copyToClipboard }
+}
