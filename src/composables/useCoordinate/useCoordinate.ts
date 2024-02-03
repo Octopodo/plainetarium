af64c7fef39efb05a544f222d8dbbeac4509c0b2
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-02-03 14:31:29 +0100

    Implemented coordinate class

---
+++ src/composables/useCoordinate/useCoordinate.ts
@@ -0,0 +1,23 @@
+import { computed } from 'vue'
+
+export interface Coordinate {
+  x?: number | string
+  y?: number | string
+}
+
+interface PureCoordinate {
+  x: number
+  y: number
+}
+
+export function useCoordinate(coord: Coordinate) {
+  const x = computed(() => Number(coord.x))
+  const y = computed(() => Number(coord.y))
+  return { x, y }
+}
+
+export function useCoordinateToPercentage(coord: PureCoordinate) {
+  const x = computed(() => `${coord.x}%`)
+  const y = computed(() => `${coord.y}%`)
+  return { x, y }
+}
