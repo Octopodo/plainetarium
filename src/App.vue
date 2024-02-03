3cf2af3e95f4df25011db83e79b820341c5c3360
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-02-03 12:16:01 +0100

    Basic playground implemented

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <script setup lang="ts">
-import PlanetLayout from '@/components/Planet/PlanetLayout.vue'
-import PlainSphere from '@/components/PlainSphere.vue'
-import LightSphere from './components/LightSphere.vue'
-import { ref } from 'vue'
-const size = ref(300)
+import LightPlayground from '@/components/playgrounds/LightPlayground.vue'
+import PlaygroundControls from '@/components/playgrounds/BasePlayground/PlaygroundControls.vue'
+import PlaygroundLayout from '@/components/playgrounds/BasePlayground/PlaygroundLayout.vue'
+
+const props = defineProps({
+  controls: Array<any>
+})
 </script>
 <template>
-  <!-- <PlanetLayout /> -->
-  <PlainSphere color="#552500" :size="size">
-    <LightSphere
-      sharpness="50"
-      distance="100"
-      opacity="100"
-      :size="Number(size)"
-      shade-color="#050520"
-    />
-  </PlainSphere>
-  <!-- <TestSphere :sharpnes="0" :opacity="50" /> -->
+  <LightPlayground />
 
-  <input type="range" class="the-range" v-model="size" min="20" max="500" />
+  <!-- <PlaygroundControls /> -->
 </template>
 
 <style scoped></style>
