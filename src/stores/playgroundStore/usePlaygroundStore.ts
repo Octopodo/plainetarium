33a4e3c5fc49016cf2c6f5c3b2a869ec4f804fa1
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-02-06 14:11:13 +0100

    Removed focused layer

--- src/stores/playgroundStore/usePlaygroundStore.ts
+++ src/stores/playgroundStore/usePlaygroundStore.ts
@@ -3,17 +3,18 @@ import { type LayerData } from '@/types'
 import { useCreateComponentModel } from '@/composables/ui'
 export const usePlaygroundStore = defineStore('playground', {
   state: () => ({
-    layers: [] as LayerData[],
-    focusedLayer: null as LayerData | null
+    layers: [] as LayerData[]
   }),
   actions: {
     addLayer(component: Object) {
       const model = useCreateComponentModel(component)
+      const index = this.layers.length
       const layer: LayerData = {
         componentModel: model,
         controls: model.controls,
         focused: false,
-        visible: true
+        visible: true,
+        index: index
       }
       layer.focused = true
       this.layers.push(layer)
