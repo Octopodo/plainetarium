c852c0f83cd34cfa18960209f5b386d252620621
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-02-13 15:58:20 +0100

    Added with and without function

--- src/utils/objectUtilities/getRandomAttribute.ts
+++ src/utils/objectUtilities/getRandomAttribute.ts
@@ -1,4 +1,5 @@
 import { Random } from 'random-js'
+import { getPartialObjectWith, getPartialObjectWithout } from '@/utils'
 const random = new Random()
 
 export function getRandomAttribute<T extends Object>(obj: T): T[keyof T] {
@@ -9,4 +10,20 @@ export function getRandomObjetctKey<T extends Object>(obj: T): keyof T {
   const keys = Object.keys(obj)
   const randomKey = random.integer(0, keys.length - 1)
   return keys[randomKey] as keyof T
+}
+
+export function getRandomAttributeWith<T extends Object>(
+  obj: T,
+  keys: string[] | string
+): T[keyof T] {
+  const partialObj = getPartialObjectWith(obj, keys)
+  return getRandomAttribute(partialObj)
+}
+
+export function getRandomAttributeWithout<T extends Object>(
+  obj: T,
+  keys: string[] | string
+): T[keyof T] {
+  const partialObj = getPartialObjectWithout(obj, keys)
+  return getRandomAttribute(partialObj)
 }
