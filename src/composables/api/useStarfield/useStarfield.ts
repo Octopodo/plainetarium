8aeeecf828008164613dd08d50847a4b1f7726eb
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-02-15 14:36:38 +0100

    Added size props

--- src/composables/api/useStarfield/useStarfield.ts
+++ src/composables/api/useStarfield/useStarfield.ts
@@ -4,19 +4,23 @@ import { type NumericProp } from '@/types'
 import { useRandomColor } from '@/composables/common'
 
 import {
+  useColor,
   useOpacity,
+  SizeProps,
   ColorProps,
   OpacityProps,
   RangeSizeProps,
   type RangeSizeParams,
   type ColorParams,
-  type OpacityParams
+  type OpacityParams,
+  type SizeParams
 } from '@/composables/api'
 
 export interface StarfieldParams
   extends RangeSizeParams,
     ColorParams,
-    OpacityParams {
+    OpacityParams,
+    SizeParams {
   count: NumericProp
 }
 
@@ -28,23 +32,9 @@ const StarfieldColorProps = {
     hideControl: true
   }
 }
-export const StarfieldProps = {
-  ...RangeSizeProps,
-  ...StarfieldColorProps,
-  ...OpacityProps,
-  count: {
-    type: [Number, String],
-    default: 1000,
-    control: 'range',
-    min: 1,
-    max: 1000
-  },
 
-  reset: {
-    type: Function,
-    default: () => {},
-    control: 'button'
-  },
+const StarfieldSizeProps = {
+  ...SizeProps,
   width: {
     type: [Number, String],
     default: 2500,
@@ -60,6 +50,25 @@ export const StarfieldProps = {
     max: 3000
   }
 }
+export const StarfieldProps = {
+  ...StarfieldSizeProps,
+  ...RangeSizeProps,
+  ...StarfieldColorProps,
+  ...OpacityProps,
+  count: {
+    type: [Number, String],
+    default: 1000,
+    control: 'range',
+    min: 1,
+    max: 1000
+  },
+
+  reset: {
+    type: Function,
+    default: () => {},
+    control: 'button'
+  }
+}
 export function useStarfield(props: Required<StarfieldParams>) {
   let stars
   let regenerate
@@ -73,21 +82,18 @@ export function useStarfield(props: Required<StarfieldParams>) {
 function generateStars(props: Required<StarfieldParams>) {
   const stars = [] as any[]
   const random = new Random()
+  const { saturation, lightness } = useColor(props)
+  const { opacity } = useOpacity(props)
   const starCount = computed(() => Number(props.count))
-  const saturation = computed(() => Number(props.saturation))
   const minSize = computed(() => Number(props.minSize))
   const maxSize = computed(() => Number(props.maxSize))
 
   // const sizeRange = useSafeRange(props.size)
 
   for (let i = 0; i < starCount.value; i++) {
-    const color = computed(() =>
-      props.color !== '' ? props.color : useRandomColor(saturation.value).value
-    )
+    const { color } = useColor(props)
 
-    const opacity = computed(
-      () => random.real(contrastRange.value[0], contrastRange.value[1]) / 100
-    )
+    const randomOpacity = computed(() => random.real(0, 100) / 100)
 
     const size = computed(() => random.real(minSize.value, maxSize.value))
     const x = computed(() => Math.random() * window.innerWidth)
@@ -97,7 +103,7 @@ function generateStars(props: Required<StarfieldParams>) {
       y: y,
       size: size,
       color: color,
-      opacity: opacity
+      opacity: randomOpacity.value
     }
     stars.push(star)
   }
